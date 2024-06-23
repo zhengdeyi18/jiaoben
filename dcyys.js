3ad574b066568761d081c7ddd386ded00d4e15b9
zhengdeyi18 <101560411+zhengdeyi18@users.noreply.github.com>
2024-06-23 17:06:32 +0800

    Create dcyys.js

---
+++ dcyys.js
@@ -0,0 +1,23 @@
+/*多次元雅思
+
+[rewrite_local] 
+^https?:\/\/ielts\.dcyedu\.com\/\/app\/user\/info url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/DCYYS.js
+[MITM]
+hostname = ielts.dcyedu.com
+*/
+var Rnik = JSON.parse($response.body);
+Rnik = {
+  "msg": "登录成功",
+  "data": {
+    "sex": "0",
+    "nickName": "ZDY",
+    "isMember": true,
+    "systemTime": "4092610661000",
+    "userId": 95914,
+    "avatar": "https://file.dcyedu.com/2024/05/17/03686561_58C2_4196_B6AF_BA763B984904.png"
+  },
+  "code": 200
+};
+$done({
+  "body": JSON.stringify(Rnik)
+});
