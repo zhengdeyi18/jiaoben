32b755c79ecfe07f12f29fa38828a2b7bbe2cd66
zhengdeyi18 <101560411+zhengdeyi18@users.noreply.github.com>
2024-08-24 09:39:58 +0800

    Update embyxqbh.js

--- embyxqbh.js
+++ embyxqbh.js
@@ -6,7 +6,7 @@
 ^https:\/\/.+\/emby\/Sessions\/Playing\/Stopped url script-request-body https://raw.githubusercontent.com/zhengdeyi18/jiaoben/main/embyxqbh.js
 
 [task_local]
-35 22 15,30 * * https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Emby.js, tag=Emby自动观看, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/lige47/emby.png, enabled=true
+35 22 15,30 * * https://raw.githubusercontent.com/zhengdeyi18/jiaoben/main/embyxqbh.js, tag=Emby自动观看, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/lige47/emby.png, enabled=true
 
 [MITM]
 hostname = -*.fileball.app, *
